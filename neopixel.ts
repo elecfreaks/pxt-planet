92737a768b28059b8e4bfc61bc9f87ad4f990406
lionkkkk <lion_yan@outlook.com>
2020-04-09 19:37:00 +0800

    no message

--- neopixel.ts
+++ neopixel.ts
@@ -39,7 +39,7 @@ enum NeoPixelMode {
 /**
  * Functions to operate NeoPixel strips.
  */
-//% weight=5 color=#2699BF icon="\uf110"
+//% color=#2699BF icon="\uf110"
 namespace neopixel {
     /**
      * A NeoPixel strip
--- oled.ts
+++ oled.ts
@@ -1,9 +1,7 @@
 declare interface Math {
     floor(x: number): number;
 }
-
-
-//% color=#27b0ba weight=100 icon="\uf26c"
+//% color=#27b0ba  icon="\uf26c"
 namespace OLED {
     let font: Buffer;
 
